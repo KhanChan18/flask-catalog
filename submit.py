dfb917174f434ee21d0eb433d7ac8553817437d3
Khannian <ch2013pku@gmail.com>
2017-10-01 19:48:35 +1100

    Add submit.py

---
+++ submit.py
@@ -0,0 +1,15 @@
+from requests import post
+
+def makeDict(counter):
+    res = {}
+    res['name'] = 'Product No.%s' % counter
+    res['price'] = '%s' % (0.67*(counter-0.43)**3)
+    res['category'] = 'Category No.%s' % (counter % 5)
+    return res
+
+
+if __name__=="__main__":
+
+    target = "http://localhost:5000/product-create"
+    for i in range(30):
+        post(target, makeDict(i)
